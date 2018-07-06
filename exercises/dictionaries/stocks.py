e2fbee38683ea28a9d2f1928fa071c055bfb4d61
jcarter0149 <jacobraycarter@gmail.com>
2018-07-06 14:39:18 -0500

    added the dates to the stock history

--- exercises/dictionaries/stocks.py
+++ exercises/dictionaries/stocks.py
@@ -7,10 +7,10 @@ purchases = [ ('GM', 100, '10-sep-2001', 48),
 
 for company, v in stockDict.items():
     if company == purchases[0][0]:
-        print(v, purchases[0][1] * purchases[0][3])
+        print(v, purchases[0][1] * purchases[0][3], purchases[0][2])
     elif company == purchases[1][0]:
-        print(v, purchases[1][1] * purchases[1][3])
+        print(v, purchases[1][1] * purchases[1][3], purchases[1][2])
     elif company == purchases[2][0]:
-        print(v, purchases[2][1] * purchases[2][3])
+        print(v, purchases[2][1] * purchases[2][3], purchases[2][2])
 
 print(company)
